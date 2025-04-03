fc48df1985946fa214b8fda105b1bb0d27f06d66
vipul <rajvipulraj401@gmail.com>
2025-04-03 18:06:51 +0530

    Optimized the function and tried it

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
-import React from "react";
-import { useState } from "react";
+// import React from "react";
+// import { useState } from "react";
+// const App = () => {
+//   let [value, setValue] = useState(0);
+//   return (
+//     <>
+//       <div>
+//         <h1> Counter App</h1>
+//         <h2>Count: {value}</h2>
+
+//         <div>
+//           <button onClick={() => setValue((prevValue) => (prevValue += 1))}>
+//             Increment
+//           </button>
+//           <button onClick={() => setValue((prevValue) => (prevValue -= 1))}>
+//             Decrement
+//           </button>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// ------------By using one function only to handle Submit-----
+
+import React, { useState } from "react";
+
 const App = () => {
-  let [value, setValue] = useState(0);
+  const [value, setValue] = useState(0);
+
+  const handleClick = (event) => {
+    const action = event.target.textContent; // Button ke andar ka text nikalna
+    setValue((prevValue) =>
+      action === "Increment" ? prevValue + 1 : prevValue - 1
+    );
+  };
+
   return (
     <>
       <div>
@@ -9,12 +44,8 @@ const App = () => {
         <h2>Count: {value}</h2>
 
         <div>
-          <button onClick={() => setValue((prevValue) => (prevValue += 1))}>
-            Increment
-          </button>
-          <button onClick={() => setValue((prevValue) => (prevValue -= 1))}>
-            Decrement
-          </button>
+          <button onClick={handleClick}>Increment</button>
+          <button onClick={handleClick}>Decrement</button>
         </div>
       </div>
     </>
